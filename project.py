ab697f29ef32335dad871b7d517970354fe88ebd
Smart developer <developer@yandex.ru>
2019-02-18 20:18:30 +0400

    working

--- project.py
+++ project.py
@@ -7,6 +7,12 @@ size = (400, 500)
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 shipposx = 0
+rocketposx = 0
+rocketposy = 0
+enemyshipposx = 0
+enemyshipposy = 0
+
+Rockets = pygame.sprite.Group()
 
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
@@ -34,11 +40,6 @@ class My_ship(pygame.sprite.Sprite):
         self.rect.x = 175
         self.rect.y = 400
 
-    def get_event(self, event):
-        #print('event')
-        if self.rect.collidepoint(event.pos):
-            self.image = self.image
-
     def update(self):
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_a:
@@ -49,8 +50,11 @@ class My_ship(pygame.sprite.Sprite):
                 self.rect.x += 10
                 shipposx = self.rect.x
                 My_ship.possition = shipposx
-
-
+        if self.rect.x < 0:
+            self.rect.x = -self.rect.x
+        if self.rect.x > 345:
+            self.rect.x = -self.rect.x
+                
 class Enemy_ship(pygame.sprite.Sprite):  
     star = load_image("car2.png")
     image = pygame.transform.scale(star, (50, 50))
@@ -59,17 +63,33 @@ class Enemy_ship(pygame.sprite.Sprite):
         super().__init__(group)
         self.image = Enemy_ship.image
         self.rect = self.image.get_rect()
-        self.rect.x = randint(0, 450)
+        self.rect.x = randint(0, 350)
         self.rect.y = -50
     
     def update(self):
         v = 100
         fps = 100
         self.rect.y += v / fps
-        if int(self.rect.y) == 400:
-            self.kill() 
-        clock.tick(fps)
+        if pygame.sprite.spritecollideany(self , Rockets):
+            print("тута")
 
+class Enemy_ship2(pygame.sprite.Sprite):  
+    star = load_image("car3.png")
+    image = pygame.transform.scale(star, (50, 50))
+
+    def __init__(self, group):
+        super().__init__(group)
+        self.image = Enemy_ship2.image
+        self.rect = self.image.get_rect()
+        self.rect.x = randint(0, 350)
+        self.rect.y = -50
+    
+    def update(self):
+        v = 200
+        fps = 100
+        self.rect.y += v / fps
+        #clock.tick(fps)
+    
 class Rocket(pygame.sprite.Sprite):
     Lazer = load_image("rocket.png")
     image = pygame.transform.scale(Lazer, (10, 25))
@@ -78,19 +98,18 @@ class Rocket(pygame.sprite.Sprite):
         super().__init__(group)
         self.image = Rocket.image
         self.rect = self.image.get_rect()
-
-        self.rect.x = shipposx + 22.5
-        
         self.rect.x = My_ship.possition + 22.5
-        self.rect.y = 415
+        self.rect.y = 395
+        self.add(Rockets)
     
     def update(self):
         #print(shipposx)
         v = 300
         fps = 100
         self.rect.y -= v / fps
-        clock.tick(fps)
-        
+        #if pygame.sprite.spritecollideany(self, Enemy_ship):
+        #    print('ok')
+        #clock.tick(fps)
 
 class Wall(pygame.sprite.Sprite):
     wall = load_image("space.png")
@@ -100,6 +119,23 @@ class Wall(pygame.sprite.Sprite):
         super().__init__(group)
         self.image = Wall.image
         self.rect = self.image.get_rect()  
+
+class Star2(pygame.sprite.Sprite):  
+    star2 = load_image("star2.png")
+    image = pygame.transform.scale(star2, (7, 10))
+
+    def __init__(self, group):
+        super().__init__(group)
+        self.image = Star.image
+        self.rect = self.image.get_rect()
+        self.rect.x = randint(0, 450)
+        self.rect.y = -70
+    
+    def update(self):
+        v = 400
+        fps = 100
+        self.rect.y += v / fps
+        #clock.tick(fps)
 
 class Star(pygame.sprite.Sprite):  
     star = load_image("star.png")
@@ -113,32 +149,10 @@ class Star(pygame.sprite.Sprite):
         self.rect.y = -70
     
     def update(self):
-        v = 600
+        v = 800
         fps = 100
         self.rect.y += v / fps
-        if int(self.rect.y) == 400:
-            self.kill() 
-        clock.tick(fps)
         
-
-class Star2(pygame.sprite.Sprite):  
-    star = load_image("star2.png")
-    image = pygame.transform.scale(star, (5, 5))
-
-    def __init__(self, group):
-        super().__init__(group)
-        self.image = Star.image
-        self.rect = self.image.get_rect()
-        self.rect.x = randint(0, 450)
-        self.rect.y = -70
-    
-    def update(self):
-        v = 100
-        fps = 100
-        self.rect.y += v / fps
-        clock.tick(fps)
-
-
 all_sprites = pygame.sprite.Group()
 v = 200
 fps = 100
@@ -148,29 +162,36 @@ Star(all_sprites)
 Star2(all_sprites)
 Rocket(all_sprites)
 Enemy_ship(all_sprites)
-clock = pygame.time.Clock()
+Enemy_ship2(all_sprites)
 start_ticks = pygame.time.get_ticks()
 timershowstar = 0
 timershowstar2 = 0
 rockettime = 0
+enemyship2time = 0
+enemyshiptime = 0
 running = True
 while running:
-    #print(shipposx)
     fps = int(clock.tick(fps))
     timershowstar += fps
     timershowstar2 += fps
     rockettime += fps
-    # print(fps)
-    # print(timershowstar)
+    enemyship2time += fps
+    enemyshiptime += fps
     if timershowstar > 3000 and  timershowstar < 3200:
         timershowstar = 0 
         Star(all_sprites)
     if timershowstar2 > 4000 and  timershowstar2 < 4200:
         timershowstar2 = 0 
         Star2(all_sprites)  
-    if rockettime > 2000 and rockettime < 2200:
+    if rockettime > 1700 and rockettime < 1800:
         rockettime = 0
         Rocket(all_sprites)     
+    if enemyship2time > 5000 and enemyship2time < 5100:
+        enemyship2time = 0
+        Enemy_ship2(all_sprites)
+    if enemyshiptime > 4500 and enemyshiptime < 4550:
+        enemyshiptime = 0
+        Enemy_ship(all_sprites)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
--- test.py
+++ test.py
@@ -1,51 +1,105 @@
-from random import randint
+# import os
+# import pygame
+# import random
+
+# pygame.init()
+# size = (400, 500)
+# screen = pygame.display.set_mode(size)
+
+# class Ball(pygame.sprite.Sprite):
+#     def __init__(self, radius, x, y):
+#         super().__init__(group)
+#         self.radius = radius
+#         self.image = pygame.Surface((2 * radius, 2 * radius),
+#                                     pygame.SRCALPHA, 32)
+#         pygame.draw.circle(self.image, pygame.Color("red"),
+#                            (radius, radius), radius)
+#         self.rect = pygame.Rect(x, y, 2 * radius, 2 * radius)
+#         self.vx = random.randint(-5, 5)
+#         self.vy = random.randrange(-5, 5)
+ 
+#     def update(self):
+#         self.rect = self.rect.move(self.vx, self.vy)
+
+# class Border(pygame.sprite.Sprite):
+#     def __init__(self, x1, y1, x2, y2):
+#         super().__init__(all_sprites)
+#         if x1 == x2:  # вертикальная стенка
+#             self.add(vertical_borders)
+#             self.image = pygame.Surface([1, y2 - y1])
+#             self.rect = pygame.Rect(x1, y1, 1, y2 - y1)
+#         else:  # горизонтальная стенка
+#             self.add(horizontal_borders)
+#             self.image = pygame.Surface([x2 - x1, 1])
+#             self.rect = pygame.Rect(x1, y1, x2 - x1, 1)
+
+# Border(5, 5, width - 5, 5)
+# Border(5, height - 5, width - 5, height - 5)
+# Border(5, 5, 5, height - 5)
+# Border(width - 5, 5, width - 5, height - 5)
+
+# all_sprites = pygame.sprite.Group()
+
+# for i in range(10):
+#     Ball(20, 100, 100)
+
+# Ball(all_sprites)
+# horizontal_borders = pygame.sprite.Group()
+# vertical_borders = pygame.sprite.Group()
+
+# running = True
+# while running:  
+#     all_sprites.draw(screen)
+#     all_sprites.update()
+#     pygame.display.flip()
+# pygame.quit()
+
+
+
+
+import os
 import pygame
+import random
+from random import randint
+
 pygame.init()
-pygame.time.set_timer(pygame.USEREVENT, 3000)
- 
-W = 400
-H = 300
-WHITE = (255, 255, 255)
-CARS = ('car1.png', 'car2.png', 'car3.png')
-CARS_SURF = []  # для хранения готовых машин-поверхностей
- 
-# надо установить видео режим до вызова image.load()
-sc = pygame.display.set_mode((W, 400))
- 
-for i in range(len(CARS)):
-    CARS_SURF.append(pygame.image.load(CARS[i]).convert_alpha())
- 
- 
-class Car(pygame.sprite.Sprite):
-    def __init__(self, x, surf, group):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = surf
-        self.rect = self.image.get_rect(center=(x, 0))
-        self.add(group)  # добавляем в группу
-        self.speed = randint(1, 3)  # у машин будет разная скорость
+size = (600, 300)
+Surface = pygame.display.set_mode(size)
+radius = 50
+x = randint(0, 300)
+y = randint(0, 300)
+
+class Bomb(pygame.sprite.Sprite):
+    def __init__(self, group):
+        self.radius = radius
+        self.image = pygame.Surface((2 * radius, 2 * radius),
+                                    pygame.SRCALPHA, 32)
+        pygame.draw.circle(self.image, pygame.Color("red"),
+                           (radius, radius), radius)
+        self.rect = pygame.Rect(x, y, 2 * radius, 2 * radius)
+        self.vx = random.randint(-5, 5)
+        self.vy = random.randrange(-5, 5)
  
     def update(self):
-        if self.rect.y < H:
-            self.rect.y += self.speed
-        else:
-            # теперь не перебрасываем вверх,
-            # а удаляем из всех групп
-            self.kill()
- 
-cars = pygame.sprite.Group()
- 
-# добавляем первую машину, которая появляется сразу
-Car(randint(1, W), CARS_SURF[randint(0, 2)], cars)
- 
-while 1:
-    for i in pygame.event.get():
-        if i.type == pygame.QUIT:
-            exit()
-        elif i.type == pygame.USEREVENT:
-            Car(randint(1, W), CARS_SURF[randint(0, 2)], cars)
- 
-    sc.fill(WHITE)
-    cars.draw(sc)
-    pygame.display.update()
-    pygame.time.delay(20)
-    cars.update()
+        self.rect = self.rect.move(random.randrange(3) - 1,
+                                   random.randrange(3) - 1)
+
+    def get_event(self, event):
+        if self.rect.collidepoint(event.pos):
+            self.image = self.boob
+
+all_sprites = pygame.sprite.Group()
+
+for _ in range(20):
+    Bomb(all_sprites)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False     
+        Surface.fill((0, 0, 0))
+        all_sprites.draw(Surface)
+        all_sprites.update()
+        pygame.display.flip()
+pygame.quit()
